2419ed1299d7cc3b771917122cbd9d8a16fd9fe0
Lee <fw22912@bristol.ac.uk>
2023-10-13 14:13:30 +0100

    Update

--- client/client.go
+++ client/client.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 func read(conn net.Conn) {
@@ -18,6 +18,7 @@ func main() {
 	// Get the server address and port from the commandline arguments.
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
+	fmt.Println(addrPtr)
 	//TODO Try to connect to the server
 	//TODO Start asynchronously reading and displaying messages
 	//TODO Start getting and sending user messages.
